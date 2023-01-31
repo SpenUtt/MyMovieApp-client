d3406990f23926fa1644fc8e146f49908254fc76
SpenUtt <spencerutt8@gmail.com>
2023-01-31 16:27:59 +0100

    Added integration to movie_api and prop-types

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,51 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1,
-            title: "Movie 1",
-            image: "movie1img.png",
-            director: "Movie1 Director",
-            genre: "movie1genre", 
-            details: "Movie1 description"
-        },
-        {
-            id: 2,
-            title: "Movie 2",
-            image: "movie2img.png",
-            director: "Movie2 Director",
-            genre: "movie2genre", 
-            details: "Movie2 description" 
-        },
-        {
-            id: 3,
-            title: "Movie 3",
-            image: "movie3img.png",
-            director: "Movie3 Director",
-            genre: "movie3genre", 
-            details: "Movie3 description" 
-        },
-        {
-            id: 4,
-            title: "Movie 4",
-            image: "movie4img.png",
-            director: "Movie4 Director",
-            genre: "movie4genre", 
-            details: "Movie4 description" 
-        },
-        {
-            id: 5,
-            title: "Movie 5",
-            image: "movie5img.png",
-            director: "Movie5 Director",
-            genre: "movie5genre", 
-            details: "Movie5 description" 
-        },
-    ]);
+    const [movies, setMovies] = useState([]);
+
     const [selectedMovie, setSelectedMovie] = useState(null);
+    useEffect(() => {
+        fetch("https://api-mymovieapp.onrender.com/movies")
+        .then((response) => response.json())
+        .then((data) => {
+            const moviesFromApi = data.docs.map((doc) => {
+                return {
+                    id: doc.key, 
+                    title: doc.title, 
+                    genre: doc.genre,
+                    details: doc.details,
+                    director: doc.director_name?.[0]
+                };
+            });
+            setMovies(moviesFromApi);
+        });
+    }, []);
 
     if (selectedMovie) {
         return (
